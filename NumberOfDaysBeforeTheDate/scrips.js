e1d72046256d97ad960a2b1237d25ff982c31bb3
Sterben <sergey.matushkin.98@mail.ru>
2025-05-18 11:46:08 +0300

    add calculate function

--- NumberOfDaysBeforeTheDate/scrips.js
+++ NumberOfDaysBeforeTheDate/scrips.js
@@ -2,6 +2,9 @@ const askDateContainer = document.querySelector('#ask-date-container');
 const dateNameInput = document.querySelector('#date-name-input');
 const dateInput = document.querySelector('#date-input');
 const numDaysContainer = document.querySelector('#num-days-container');
+const numDays = document.querySelector('#num-days');
+const numHours = document.querySelector('#num-hours');
+const numMinutes = document.querySelector('#num-minutes');
 const button = document.querySelector('#button');
 
 // localStorage.clear();
@@ -32,9 +35,42 @@ button.addEventListener('click', function() {
     dateArr.push(obj);
     
     localStorage.setItem('date', JSON.stringify(dateArr));
-
-    // hideAskContainer();
+    hideAskContainer();
+    calculateNumDays();
+    showNumDaysContainer();
 });
+
+setInterval(calculateNumDays, 1000)
+
+function calculateNumDays() {
+    if (jsonDate) {
+        const now = new Date();
+
+        const enteredDateProp = JSON.parse(jsonDate);
+        
+        const enteredDateValue = enteredDateProp[0].date;
+
+        const enteredDateArr = enteredDateValue.split('.');
+        
+        const enteredDay = +enteredDateArr[0];
+        const enteredMonth = +enteredDateArr[1];
+        const enteredYear = +enteredDateArr[2];
+
+        const enteredDate = new Date(enteredYear, enteredMonth -1, enteredDay);
+
+        let diff = enteredDate.getTime() - now.getTime();
+        let numDaysBeforeDate = (diff / (1000 * 60 * 60 * 24)).toFixed(2);
+        let numHoursBeforeDate = Math.floor(diff / (1000 * 60 * 60));
+        let numSecondsBeforeDate = Math.floor(diff / (1000 * 60));
+
+        numDays.textContent = `Кол-во дней до даты: 
+        дней: ${numDaysBeforeDate}`;
+        numHours.textContent = `Часов: ${numHoursBeforeDate}`;
+        numMinutes.textContent = `Минут: ${numSecondsBeforeDate}`;
+    }
+    
+};
+
 
 function hideAskContainer() {
     askDateContainer.classList.add('hide');
